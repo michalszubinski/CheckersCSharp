80b793fe121a5d285fa4f98aa367b4bf8b888078
michalszubinski <mich999.pl@gmail.com>
2024-01-12 13:41:19 +0100

    Working on 'textToCoordinates'

--- Checkers/Base/Coordinates.cs
+++ Checkers/Base/Coordinates.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Checkers.Base
@@ -16,13 +17,32 @@ namespace Checkers.Base
             x =argx; y = argy;    
         }
 
-        public static Coordinates textToCoordinates(string text)
+        public static List<Coordinates> textToCoordinates(string text) // CAN THROW 'InvalidCastException'
         {
-            Coordinates cords = new Coordinates();
+            List<Coordinates> cordList = new List<Coordinates>();
 
-            // TODO
+            //TODO: Expand functionality beyond [a-z][1-9]
+            //TODO: Add support for multiple character x coordinates, for example: aaaaaaaa4
+            //TODO: Add support for multiple character y coordinates, for example: f12
+            //TODO: Add support to reverse coordinates, for example: 4g
+            //TODO: Accept input with spaces, for example: 'c 4';  ' c4'; 'c4 '
+            //TODO: Support uppercase characters
+            //TODO: Return list of coordinates
 
-            return cords;
+            MatchCollection matches = Regex.Matches(text, $"[a-z][1-9]", RegexOptions.Singleline);
+
+            if (matches.Count == 0) throw new InvalidCastException("ERROR: Coordinates.textToCoordinates(string): No matches for given text");
+
+            foreach (Match match in matches)
+            {
+                Coordinates temp = new Coordinates();
+                int.TryParse(match.Value[0], out temp.x); // TODO
+                int.TryParse(match.Value[1], out temp.y); // TODO
+
+                cordList.Add(temp);
+            }
+
+            return cordList;
         }
         
         public static Coordinates operator +(Coordinates left, Coordinates right)
